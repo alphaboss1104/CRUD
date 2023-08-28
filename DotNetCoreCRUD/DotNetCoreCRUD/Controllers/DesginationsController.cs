5a89361bc7f2e061f9d315e75aeedb66e4a7407a
alphaboss1104 <alphaboss1104@gmail.com>
2023-08-28 19:46:21 +0530

    DotNetCore - Created employees API with all CRUD operations.

---
+++ DotNetCoreCRUD/DotNetCoreCRUD/Controllers/DesginationsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DotNetCoreCRUD.Models;
+
+namespace DotNetCoreCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesginationsController : ControllerBase
+    {
+        private readonly CRUDDBContext _context;
+
+        public DesginationsController(CRUDDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Desginations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Desgination>>> GetDesginations()
+        {
+            return await _context.Desginations.ToListAsync();
+        }
+    }
+}
