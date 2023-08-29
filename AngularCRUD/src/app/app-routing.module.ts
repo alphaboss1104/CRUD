e226a3bde17cc00626151e1d3d91a02acf91ca3d
alphaboss1104 <alphaboss1104@gmail.com>
2023-08-29 15:40:56 +0530

    Angular - Added routing, and cleared home page.

--- AngularCRUD/src/app/app-routing.module.ts
+++ AngularCRUD/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeEntryComponent } from './employee-entry/employee-entry.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'employee', component:EmployeeComponent, children:[
+    {path:'', component:EmployeeListComponent},
+    {path:'create', component:EmployeeEntryComponent},
+    {path:'edit/:id', component:EmployeeEntryComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
