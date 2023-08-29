49261d48d02a31542708d34c19d107205377882a
alphaboss1104 <alphaboss1104@gmail.com>
2023-08-29 15:29:59 +0530

    Angular - Added required components

--- AngularCRUD/src/app/app.module.ts
+++ AngularCRUD/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeEntryComponent } from './employee-entry/employee-entry.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmployeeComponent,
+    EmployeeEntryComponent,
+    EmployeeListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
